2f3436d444313b3364a4bb107d83cfe8aae68d14
Pebl <komputerstart@gmail.com>
2016-05-16 13:45:07 +0000

    integracja Amazon S3 z Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.default_url_options = {host: 'pierwsza1.herokuapp.com'}
   config.paperclip_defaults = {
-    :storage => s3,
+    :storage => :s3,
     :s3_credentials => {
       :bucket => ENV['AWS_BUCKET'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
