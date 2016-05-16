dd8c579ebba6d276b9a674fbe0a013b836d86c15
Pebl <komputerstart@gmail.com>
2016-05-16 14:16:34 +0000

    Integracja z amanon S3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.default_url_options = {host: 'pierwsza1.herokuapp.com'}
-config.paperclip_defaults = {
+  config.paperclip_defaults = {
   storage: :s3,
   s3_credentials: {
     bucket: ENV.fetch('S3_BUCKET_NAME'),
